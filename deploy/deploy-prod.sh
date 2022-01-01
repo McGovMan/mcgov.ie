1e6cbaa3badccd15b39dd564b2ed35e392697681
Conor Mc Govern <conor@mcgov.ie>
2022-01-01 14:59:03 +0000

    adding deploy script

--- deploy/deploy-prod.sh
+++ deploy/deploy-prod.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-cat <<EOF > ~/test.txt
-testing that this is run
-EOF
+
+# Stop the docker container currently running the site
+echo -e "\n > Shutting down mcgov.ie"
+docker-compose -f docker-compose.prod.yml down
+
+# Pull down new updates from branch main
+echo -e "\n > Pulling down new updates from main"
+git checkout main
+git pull 
+
+# Start the service again
+echo -e "\n > Starting mcgov.ie"
+docker-compose -f docker-compose.prod.yml up -d
